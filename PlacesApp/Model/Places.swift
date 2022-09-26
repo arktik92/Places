31273b5a1450c9ed1f73b3b4925fd0d5e71ed03b
Esteban SEMELLIER <arktik92@MacBook-Pro-de-Esteban.local>
2022-09-26 09:52:47 +0200

    Fist Commit

---
+++ PlacesApp/Model/Places.swift
@@ -0,0 +1,8 @@
+//
+//  Places.swift
+//  PlacesApp
+//
+//  Created by Esteban SEMELLIER on 26/09/2022.
+//
+
+import Foundation
---
+++ PlacesApp/PlacesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesAppApp.swift
+//  PlacesApp
+//
+//  Created by Esteban SEMELLIER on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlacesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
